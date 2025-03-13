38a4785cb3052fb21304f616e0077f7efb0e28db
= <davicandidopucminas@gmail.com>
2025-03-13 15:09:57 -0300

    Update Amarrações.cpp

--- Estudos/Ex4/Amarrações.cpp
+++ Estudos/Ex4/Amarrações.cpp
@@ -1,6 +1,6 @@
 // 1) Definição de amarração;
 
-//  Amarrações podem ser definidas como uma vinculação, ou seja, a ação de dar um um identificador a um processo (função ou objeto), valor (variável) ou a um tipo de dado, ou endereço de memoria.
+//  Amarrações (ou binding) podem ser definidas como o processo de vinculação, ou seja, a ação de dar um identificador a um elemento do programa seja uma função, objeto, valor (variável) ou a um tipo de dado, ou endereço de memoria.
 
 // 2) "LP favorita";
 
@@ -8,13 +8,25 @@
 
 // 3) trecho de código com algum tipo de declaração, em sua "LP favorita".
 
+/* 
+Este exemplo mistura alguns tipos de amarrações, como na ocorrida
+na POO (Amarrações de classe), onde o bloco que contem a classe é vinculada/amarrada a 
+"Calculadora", que por sua vez possui funções que também contem suas próprias
+ amarrações (Amarrações de Método), ou seja, possuem seus identificadores interligados a classe calculadora
+ Amarrações de Objeto, onde ao instanciar Calculadora como cal, associa-se
+ "Calculadora" ao identificador "cal" por todo o bloco da main,
+ E as amarrações de tipo e variáveis, atribuindo/vinculando nomes
+ as variáveis que também são amarradas a um tipo  
+*/
+
 #include <iostream>
 
 using namespace std;
 
+// Temos aqui um exemplo de amarração de classe
 class Calculadora{
   public : 
-    int soma(int a, int b){ return a+b; }
+    int soma(int a, int b){ return a+b; } // Aqui um exemplo de amarração de Método
     int subtracao(int a, int b){ return a-b; }
     int multiplicacao(int a, int b){ return a*b; }
     int divisao(int a, int b){ return a/b; }
@@ -22,7 +34,7 @@ class Calculadora{
 
 int main(){
 
-  Calculadora cal;
+  Calculadora cal; // Amarração de Objeto
 
   cout << "Escolha uma operação sendo a=20 e b=10 : \n";
   cout << "1 - Soma\n";
@@ -31,7 +43,7 @@ int main(){
   cout << "4 - Divisão\n";
   cout << "opção: ";
   
-  int opcao;
+  int opcao; // Amarração de variável e tipo
   cin >> opcao;
 
   int result;
